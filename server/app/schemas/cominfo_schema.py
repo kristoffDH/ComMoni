b6c3af531913e34f2223781c35c4503725dcc36e
kristoffDH <dhkristoff@gmail.com>
2023-10-28 21:55:47 +0900

    updatte DB변경에 의한 schema 수정

--- server/app/schemas/cominfo_schema.py
+++ server/app/schemas/cominfo_schema.py
@@ -5,20 +5,32 @@ from pydantic import BaseModel
 
 
 class ComInfoBase(BaseModel):
-    server_id: int
-    make_datetime: Optional[datetime] = None
+    """
+        Attributes
+            - host_id : 호스트 아이디
+            - cpu_utilization : cpu 사용률
+            - memory_utilization : 메모리 사용률
+            - disk__utilization : 디스크 사용률
+            - make_datetime : 데이터 생성 날짜/시간
+    """
+    host_id: Optional[int] = None
     cpu_utilization: Optional[float] = None
     memory_utilization: Optional[float] = None
-    disk_utilization: Optional[float] = None
+    disk__utilization: Optional[float] = None
+    make_datetime: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
 
 
 class ComInfo(ComInfoBase):
-    class Config:
-        from_attributes = True
+    pass
+
+
+class ComInfoGet(ComInfoBase):
+    host_id: int
 
 
 class ComInfoCreate(ComInfoBase):
+    host_id: int
     make_datetime: datetime
-
-    class Config:
-        from_attributes = True
--- server/app/schemas/commange_schema.py
+++ server/app/schemas/commange_schema.py
@@ -4,17 +4,34 @@ from pydantic import BaseModel
 
 
 class ComManageBase(BaseModel):
-    server_name: Optional[str] = None
-    server_ip: Optional[str] = None
-    server_memory: Optional[str] = None
-    server_disk: Optional[str] = None
+    """
+        Attributes
+            - host_id : 호스트 아이디
+            - user_id : 사용자 아이디
+            - host_name : 호스트 이름
+            - host_ip : 호스트 아이피
+            - memory : 메모리 용량
+            - disk : 디스크 용량
+    """
+    host_id: Optional[int] = None
+    user_id: Optional[int] = None
+    host_name: Optional[str] = None
+    host_ip: Optional[str] = None
+    memory: Optional[str] = None
+    disk: Optional[str] = None
+
+    class Config:
+        from_attributes = True
 
 
 class ComManage(ComManageBase):
-    class Config:
-        from_attributes = True
+    pass
 
 
 class ComManageCreate(ComManageBase):
-    class Config:
-        from_attributes = True
+    host_id: int
+
+
+class ComManageUpdate(ComManageBase):
+    host_id: int
+    user_id: int
