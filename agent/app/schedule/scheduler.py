f25127729cb7e87de0afa304ee0410c055b3c6ca
kristoffDH <dhkristoff@gmail.com>
2023-11-05 23:02:34 +0900

    [Agent]실시간 ComInfo 서버로 전송 기능 구현

---
+++ agent/app/schedule/scheduler.py
@@ -0,0 +1,35 @@
+from typing import Any, List, Optional
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+class Scheduler:
+    """
+    AsyncIOScheduler를 이용한 스케줄러 클래스
+    """
+
+    def __init__(self):
+        """
+        생성자. 스케줄러 객체 생성 및 실행
+        """
+        self.__scheduler = AsyncIOScheduler()
+        self.__scheduler.start()
+
+    def shutdown(self) -> None:
+        """
+        스케줄러에 등록된 모든 job 정지
+        :return:
+        """
+        if self.__scheduler.running:
+            self.__scheduler.shutdown()
+        else:
+            print(f"scheduler is not running state")
+
+    def add_cron_schedule(self, func: Any, second: str, args: List = None) -> None:
+        """
+        스케줄러에 job 추가 (cron 형태만 사용)
+        :param func: 호춣될 함수
+        :param second: 호출 주기
+        :param args: 호출될 함수에 전달할 파라미터
+        :return: None
+        """
+        self.__scheduler.add_job(func=func, trigger="cron", second=second, args=args)
---
+++ agent/app/sig/setting.py
@@ -0,0 +1,28 @@
+import asyncio
+from asyncio.events import AbstractEventLoop
+from signal import SIGINT, SIGTERM
+
+
+def handler(sig):
+    """
+    signal 처리를 위한 handler 함수
+    :param sig: 프로세스에서 받은 시그널
+    :return: None
+    """
+    loop = asyncio.get_running_loop()
+
+    for task in asyncio.all_tasks(loop=loop):
+        task.cancel()
+
+    loop.remove_signal_handler(SIGTERM)
+    loop.add_signal_handler(SIGINT, lambda: None)
+
+
+def set_signal(loop: AbstractEventLoop):
+    """
+    현재 동작중인 eventloop에 signal 처리 등록
+    :param loop:
+    :return:
+    """
+    for sig in [SIGTERM, SIGINT]:
+        loop.add_signal_handler(sig, handler, sig)
